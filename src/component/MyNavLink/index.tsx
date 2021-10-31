8eec38b2354eaec72a138e0ae933109133c42c08
s-elo <zz321yy@qq.com>
2021-10-31 14:06:06 +0800

    delete 07-router

--- src/component/MyNavLink/index.tsx
+++ src/component/MyNavLink/index.tsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 // import "./index.css";
 
-export default class MyNavLink extends Component {
+type Props = {
+  to: string;
+};
+
+export default class MyNavLink extends Component<Props> {
   render() {
     // this.props includes the children prop which is the content of the link
     return <NavLink activeClassName="my-active" {...this.props} />;

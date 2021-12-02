c4e4704f392a6516cbe465cded772185c2f3d6eb
s-elo <zz321yy@qq.com>
2021-12-02 11:41:26 +0800

    createElement func in Simact

--- Simact/index.js
+++ Simact/index.js
@@ -1,15 +1,51 @@
 const rootDom = document.querySelector("#root");
 
-// const element = React.createElement("div", null, "div created by raw React");
-const element = <div>div created by JSX</div>;
-ReactDOM.render(element, rootDom);
+// const rElement = React.createElement(
+//   "div",
+//   { id: "Text" },
+//   React.createElement("h1", null, "title"),
+//   React.createElement("p", null, "text")
+// );
+// console.log(rElement);
+// const element = <div>div created by JSX</div>;
+// ReactDOM.render(element, rootDom);
 
 const Simact = {
   createElement,
+  render
 };
 
-Simact.createElement();
+const element = Simact.createElement(
+  "div",
+  { id: "Text" },
+  Simact.createElement("h1", null, "title"),
+  Simact.createElement("p", null, "text")
+);
 
-function createElement(tagName = "div", props = {}, ...children) {
-  console.log("created");
+Simact.render(element, rootDom);
+
+function createElement(type = "div", props = {}, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function render(element, container) {
+  console.log(element);
 }

c74fd63b075347232dd47bd0153b9619d2fbb2e3
s-elo <zz321yy@qq.com>
2022-07-16 09:25:41 +0800

    fix: :bug: github page router
    change to hash mode for the router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 const rootDom = document.getElementById("root");
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   rootDom
 );
